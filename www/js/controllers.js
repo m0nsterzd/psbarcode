23313627e5dd03b1bac23d2dd715bf308bd09e11
Donald Hobbs <donald@graymata.com>
2014-11-11 21:59:09 +0200

    first commit for powerspeed project

---
+++ www/js/controllers.js
@@ -0,0 +1,122 @@
+angular.module('starter.controllers', [])
+
+.controller('ScanCtrl', function($scope, appServices, $http) {
+    $scope.message = '';
+    $scope.branch_name = '';
+    if (typeof(Storage) != "undefined") {
+        $scope.branch_name = localStorage.getItem("branch_name");
+    } else {
+        alert("Sorry, your browser does not support Web Storage...");
+    }
+    $scope.click = function() {
+        var promise = appServices.scanBarcode();
+        promise.then(
+            function(result) {
+                if (result.error == false) {
+                    var d = new Date();
+                    $scope.message = ' <div class = "list">' +
+                        '<div class = "item ng-binding">' +
+                        '<strong> Prodcode &nbsp; </strong>' + result.st_prodcode + '</div> <div class = "item ng-binding" >' +
+                        '<strong> Description &nbsp; </strong>' + result.st_sdesc + '</div> < div class = "item ng-binding" >' +
+                        '<strong> List Price&nbsp; </strong>' + result.st_list + '</div> < div class = "item ng-binding" >' +
+                        '<strong> SOH &nbsp; </strong>' + result.st_soh + '</div> </div>'
+                } else {
+                    $scope.message = '<b>ERROR</b>: ' + result;
+                }
+            },
+            function(result) {
+                $scope.message = '' + result.error;
+            },
+            function(result) {
+                $scope.message = '' + result.error;
+            });
+    }
+
+    $scope.clear = function() {
+        $scope.message = '';
+    }
+})
+
+.controller('AboutCtrl', function($scope) {
+    // if (typeof(Storage) != "undefined") {
+    //     // localStorage.setItem("firstname", "Nic");// localStorage.setItem("lastname", "Raboy");
+
+    //     alert(localStorage.getItem("firstname") + " " + localStorage.getItem("lastname"));
+    // } else {
+    //     alert("Sorry, your browser does not support Web Storage...");
+    // }
+
+})
+
+.controller('BranchesCtrl', function($scope, todoDb, $rootScope, $location) {
+    $scope.branches = [];
+    $scope.online = false;
+    $scope.syncstatus = "Offline";
+
+    $scope.toggleOnline = function() {
+        $scope.online = !$scope.online;
+        if ($scope.online) { // Read http://pouchdb.com/api.html#sync
+            $scope.syncstatus = "Online";
+            var filter = function(doc, req) {
+                if (doc.type && doc.type == 'branch') {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            $scope.sync = todoDb.replicate.from('http://192.168.100.220:5984/psmobile')
+                .on('complete', function(info) {
+                    alert('Replication complete');
+                    get_branches();
+                })
+                .on('error', function(err) {
+                    console.log("Syncing stopped");
+
+                    $scope.$apply(function() {
+                        $scope.syncstatus = err.statusText;
+                    });
+                    console.log(err);
+                });
+        } else {
+            $scope.sync.cancel();
+            $scope.syncstatus = "Offline";
+        }
+    };
+    var get_branches = function() {
+        function map(doc) {
+            emit(null, doc);
+        }
+        todoDb.query({
+            map: map
+        }, {
+            reduce: false
+        }, function(err, response) {
+            console.log(response);
+            angular.forEach(response.rows, function(value, key) {
+                console.log(value);
+                $scope.branches.push(value.value);
+            })
+            $scope.$apply(function() {
+                $scope.branches = $scope.branches;
+            });
+
+        })
+    }
+
+    get_branches();
+
+
+
+    $scope.branchSelect = function(branch) {
+        if (typeof(Storage) != "undefined") {
+            localStorage.setItem("server_ip", branch.server_ip);
+            localStorage.setItem("branch_name", branch.branch_name);
+            // localStorage.setItem("lastname", "Raboy");
+
+            alert('Server set as ' + branch.branch_name + ' ' + localStorage.getItem("server_ip"));
+        } else {
+            alert("Sorry, your browser does not support Web Storage...");
+        }
+    }
+
+})
---
+++ www/js/services.js
@@ -0,0 +1,42 @@
+angular.module('starter.services', [])
+
+.service('appServices', function appServices($q, $http) {
+    // Wrap the barcode scanner in a service so that it can be shared easily.
+    this.scanBarcode = function() {
+        // The plugin operates asynchronously so a promise
+        // must be used to display the results correctly.
+        var deferred = $q.defer();
+        try {
+            cordova.plugins.barcodeScanner.scan(
+                function(result) { // success
+                    var barcode = parseInt(result.text);
+                    var code = '6996699007328';
+                    $http.get('http://' + localStorage.getItem("server_ip") + '/positiv/index.php/products/get_ean/' + code).
+                    success(function(data, status, headers, config) {
+                        var record = data.rows[0].value;
+                        deferred.resolve({
+                            'error': false,
+                            'result': data
+                        });
+                    }).
+                    error(function(data, status, headers, config) {
+                        // log error
+                    });
+
+                },
+                function(error) { // failure
+                    deferred.resolve({
+                        'error': true,
+                        'result': error.toString()
+                    });
+                }
+            );
+        } catch (exc) {
+            deferred.resolve({
+                'error': true,
+                'result': 'exception: ' + exc.toString()
+            });
+        }
+        return deferred.promise;
+    };
+});
